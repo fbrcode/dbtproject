5f0952403096d9db3c3789dd3ee151b8bae81310
fbrcode <fabio.bressler@gmail.com>
2024-08-20 20:33:05 -0300

    fix: usage statement

--- models/bireport.sql
+++ models/bireport.sql
@@ -1,9 +1,11 @@
-{{config(
+{{
+  config(
     materialized='table',
     post_hook=["
-        GRANT USAGE ON {{target.schema}} TO GROUP bi_users;
+        GRANT USAGE ON SCHEMA {{target.schema}} TO GROUP bi_users;
         GRANT SELECT ON TABLE {{target.schema}}.bireport to group bi_users;
     "]
-)}}
+  )
+}}
 
 select * from {{ref('joins')}}
