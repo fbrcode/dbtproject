b0b27cad9578bafd6a9909b246340452359d33dc
Fabio Bressler <fabio.bressler@gmail.com>
2024-08-20 23:27:45 +0000

    add post hook

--- models/bireport.sql
+++ models/bireport.sql
@@ -1,1 +1,9 @@
+{{config(
+    materialized='table',
+    post_hook=["
+        GRANT USAGE ON {{target.schema}} TO GROUP bi_users;
+        GRANT SELECT ON TABLE {{target.schema}}.bireport to group bi_users;
+    "]
+)}}
+
 select * from {{ref('joins')}}
